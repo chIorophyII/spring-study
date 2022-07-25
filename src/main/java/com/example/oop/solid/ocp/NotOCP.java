5602c76def46e7578c3871263956fe703ab219b3
unknown <dab2in79@gmail.com>
2022-07-25 13:37:13 +0900

    ocp

--- src/main/java/com/example/oop/solid/ocp/NotOCP.java
+++ src/main/java/com/example/oop/solid/ocp/NotOCP.java
@@ -1,6 +1,10 @@
 package com.example.oop.solid.ocp;
 
+import com.example.oop.user.User;
+import com.example.oop.user.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
@@ -8,7 +12,7 @@ import java.security.NoSuchAlgorithmException;
 
 public class NotOCP {
     @Component
-    public class SHA256PasswordEncoder_ {
+    public static class SHA256PasswordEncoder_ {
 
         private final static String SHA_256 = "SHA-256";
 
@@ -37,6 +41,24 @@ public class NotOCP {
             }
 
             return hexString.toString();
+        }
+    }
+
+    @Service
+    @RequiredArgsConstructor
+    public static class UserService_notocp {
+
+        private final UserRepository userRepository;
+        private final SHA256PasswordEncoder_ passwordEncoder;
+
+        public void addUser(final String email, final String pw) {
+            final String encryptedPassword = passwordEncoder.encryptPassword(pw);
+
+            final User user = User.builder()
+                    .email(email)
+                    .password(encryptedPassword).build();
+
+            userRepository.save(user);
         }
     }
 }
--- src/main/java/com/example/oop/solid/ocp/OCP.java
+++ src/main/java/com/example/oop/solid/ocp/OCP.java
@@ -6,23 +6,51 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class OCP {
     public interface PasswordEncoder {
         String encryptPassword(final String pw);
     }
 
     @Component
-    public class SHA256PasswordEncoder implements PasswordEncoder {
+    public static class SHA256PasswordEncoder implements PasswordEncoder {
+        private final static String SHA_256 = "SHA-256";
 
         @Override
         public String encryptPassword(final String pw) {
-            return null;
+            final MessageDigest digest;
+            try {
+                digest = MessageDigest.getInstance(SHA_256);
+            } catch (NoSuchAlgorithmException e) {
+                throw new IllegalArgumentException();
+            }
+
+            final byte[] encodedHash = digest.digest(pw.getBytes(StandardCharsets.UTF_8));
+
+            return bytesToHex(encodedHash);
+        }
+
+        private String bytesToHex(final byte[] encodedHash) {
+            final StringBuilder hexString = new StringBuilder(2 * encodedHash.length);
+
+            for (final byte hash : encodedHash) {
+                final String hex = Integer.toHexString(0xff & hash);
+                if (hex.length() == 1) {
+                    hexString.append('0');
+                }
+                hexString.append(hex);
+            }
+
+            return hexString.toString();
         }
     }
 
     @Service
     @RequiredArgsConstructor
-    public class UserService {
+    public static class UserService_ocp {
 
         private final UserRepository userRepository;
         private final PasswordEncoder passwordEncoder;
