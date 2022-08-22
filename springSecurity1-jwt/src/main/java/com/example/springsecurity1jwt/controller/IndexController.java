49360abc1956fa0b20f529362e8b9e3cfdc0fa09
unknown <dab2in79@gmail.com>
2022-08-22 11:32:17 +0900

    회원가입까지

--- springSecurity1-jwt/src/main/java/com/example/springsecurity1jwt/controller/IndexController.java
+++ springSecurity1-jwt/src/main/java/com/example/springsecurity1jwt/controller/IndexController.java
@@ -1,14 +1,24 @@
 package com.example.springsecurity1jwt.controller;
 
+import com.example.springsecurity1jwt.model.Users;
+import com.example.springsecurity1jwt.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequiredArgsConstructor
 public class IndexController {
-    @GetMapping({"","/"})
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @GetMapping({"", "/"})
     public String index() {
         // 머스테치 기본폴더 src/main/resources/
         // view resolver 설정 : templates (prefix), .mustache (suffirx)
@@ -32,19 +42,24 @@ public class IndexController {
 
     // spring security가 먼저 낚아 챔
     // SecurityConfig 파일 생성 후 작동 안함
-    @GetMapping("/login")
-    public String login() {
-        return "login";
+    @GetMapping("/loginForm")
+    public String loginForm() {
+        return "loginForm";
     }
 
-    @GetMapping("/join")
-    public String join() {
-        return "join";
+    @GetMapping("/joinForm")
+    public String joinForm() {
+        return "joinForm";
     }
 
-    @PostMapping("/joinProc")
-    @ResponseBody
-    public String joinProc() {
-        return "회원가입 완료";
+    @PostMapping("/join")
+    public String join(Users user) {
+        user.setRole("ROLE_USER");
+
+        String rawPassword = user.getPassword();
+        String encPassword = bCryptPasswordEncoder.encode(rawPassword);
+        user.setPassword(encPassword);
+        userRepository.save(user);
+        return "redirect:/loginForm";
     }
 }
