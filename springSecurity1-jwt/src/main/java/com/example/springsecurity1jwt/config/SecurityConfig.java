134db98d03324e92ac4d8847771b6e240bf482cb
unknown <dab2in79@gmail.com>
2022-08-22 17:35:24 +0900

    [#1] 로그인 오류

--- springSecurity1-jwt/src/main/java/com/example/springsecurity1jwt/config/SecurityConfig.java
+++ springSecurity1-jwt/src/main/java/com/example/springsecurity1jwt/config/SecurityConfig.java
@@ -21,12 +21,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.authorizeRequests()
-                .antMatchers("/user/**").authenticated() // 권한이 필요하다
+                .antMatchers("/user/**").authenticated() // 권한이 필요하다 인증만 되면 들어갈 수 있는 주소
                 .antMatchers("/manager/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')")
                 .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
                 .anyRequest().permitAll()
                 .and()
                 .formLogin()
-                .loginPage("/loginForm");
+                .loginPage("/loginForm")
+                .loginProcessingUrl("/login") // /login 주소가 호출되면 시큐리티가 낚아채서 로그인 진행
+                .defaultSuccessUrl("/"); // 로그인 성공시
     }
 }
