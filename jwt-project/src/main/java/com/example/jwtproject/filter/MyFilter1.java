597e00946e6796ef6659e8dd133b94603b529692
unknown <dab2in79@gmail.com>
2022-08-30 18:24:39 +0900

    filter

--- jwt-project/src/main/java/com/example/jwtproject/filter/MyFilter1.java
+++ jwt-project/src/main/java/com/example/jwtproject/filter/MyFilter1.java
@@ -4,28 +4,14 @@ import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class MyFilter1 implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
-
-        HttpServletRequest req = (HttpServletRequest) request;
-        HttpServletResponse res = (HttpServletResponse) response;
-
-//        System.out.println(req.getMethod());
-        // 토큰 : 코스
-        if (req.getMethod().equals("POST")) {
-            System.out.println("POST 요청됨");
-            String headerAuth = req.getHeader("Authorization");
-            System.out.println(headerAuth);
-        }
-
-//        String headerAuth = req.getHeader("Authorization");
-//        System.out.println(headerAuth);
-
         System.out.println("필터1");
-        chain.doFilter(req, res);
+
     }
 }
--- jwt-project/src/main/java/com/example/jwtproject/filter/MyFilter3.java
+++ jwt-project/src/main/java/com/example/jwtproject/filter/MyFilter3.java
@@ -1,14 +1,39 @@
 package com.example.jwtproject.filter;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class MyFilter3 implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
-        System.out.println("필터3");
-        chain.doFilter(request, response);
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+
+//        System.out.println(req.getMethod());
+        // 토큰 : 코스
+        if (req.getMethod().equals("POST")) {
+            System.out.println("POST 요청됨");
+            String headerAuth = req.getHeader("Authorization");
+            System.out.println(headerAuth);
+            System.out.println("필터3");
+
+            if (headerAuth.equals("cos")) {
+                chain.doFilter(req, res);
+            } else {
+                PrintWriter out = res.getWriter();
+                out.println("인증 안됨");
+            }
+        }
+
+//        String headerAuth = req.getHeader("Authorization");
+//        System.out.println(headerAuth);
+
+//        System.out.println("필터1");
+//        chain.doFilter(req, res);
     }
 }
